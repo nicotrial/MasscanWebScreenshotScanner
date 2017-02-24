2bf3c74cada2b6bce3a41e2ce50c906de5d93254
root <root@localhost.localdomain>
2017-02-24 21:33:13 +0000

    Cant go without bannergit add screenscan.py

--- screenscan.py
+++ screenscan.py
@@ -2,14 +2,34 @@ import time
 import os
 import argparse # for args and shit
 from selenium import webdriver
+from random import randint
 
-driver = webdriver.Firefox()
+color = "\033["+str(randint(90,99))+"m"
+banner = u''''''+color+'''
+
+             ░░            ░▒▒▒▒▒▒░                          ▓▓▓▓
+   ░░       ░▒░            ▒▓▒▒▒▒▓▒               ▓▓▓        ▓▓▓▓
+   ▒▓░      ▒▒  ░▒▒▒▒▒▒░   ▒▒    ▒▒             ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+   ░▓▒  ░░  ▒▓░ ░▓▓▒▒▒▒░   ▒▒ ░▒▒▒░             ▓             ▓▓▓▓▓
+    ▒▒  ▓█░ ░▓▒  ▒▓▓▓▒░    ░▓████▓▒▒▒▒▒░      ▓▓▓▓ ▓  ▓▓▓▓▓▓▓     ▓▓▓▓
+    ▒▒ ░██▓░ ▒▒  ▓███▒░     ▓██▓▒▒▒▒▒▒██░    ▓  ▓▓ ▓  ▓▓    ▓     ▓  ▓▓
+    ▒▓▒▓▓▒▓▓░▒▒  ▓█▒░       ▒▒      ░▒██░   ▓▓  ▓▓ ▓  ▓▓▓▓▓▓▓     ▓   ▓
+    ░▓▓▒░ ░▓▓█▓  ▒▒         ▒▓▒▒▒▒▒▒▓▓▒░    ▓   ▓▓ ▓              ▓   ▓
+     ░░    ░▒█▓ ░▓▓▒▒▒▒▒▒░  ▒▓▒▒▒▒▒▒▒░  ▓▓▓▓▓   ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓   ▓▓▓▓▓
+             ░░ ░▒▒▒▒▒▒▒▒░  ░░
+             
+                                                                     
+\033[0m'''
+
+driver = 0
 
 def function1(keyfile):
+	global driver	
 	print("Scanning on " + keyfile)
 	os.system("masscan -p80 "+ keyfile + "> ips.txt" )
 	os.system("grep -oP '(?<=on )\S*' ips.txt > ips2.txt")
 	file = open("ips2.txt", "r") 
+	driver = webdriver.Firefox()	
 	driver.set_window_size(1024, 768) # set the window size that you need 	
 	for line in file: 
 		print (line) 
@@ -32,4 +52,5 @@ def main():
 		function1(args.i)	
 	
 if __name__ == "__main__":
+	print(banner)
 	main()
